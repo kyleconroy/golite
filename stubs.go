3a1f7075ec6eea8a58e2d59244299f294690e7db
Kyle Conroy <kyle@conroy.org>
2022-04-23 22:02:35 -0700

    Getting closer to building

---
+++ stubs.go
@@ -0,0 +1,42 @@
+package internal
+
+// No-op functions that are currently used from parse.y
+func sqlite3ExprDelete(db *sqlite3, p *Expr) {}
+
+func sqlite3ExprListDelete(db *sqlite3, p *ExprList) {}
+
+func sqlite3SelectDelete(db *sqlite3, p *Select) {}
+
+func sqlite3SrcListDelete(db *sqlite3, p *SrcList) {}
+
+func sqlite3WithDelete(db *sqlite3, p *With) {}
+
+func sqlite3WindowListDelete(db *sqlite3, p *Window) {}
+
+func sqlite3WindowDelete(db *sqlite3, p *Window) {}
+
+func sqlite3DeleteTriggerStep(db *sqlite3, p *TriggerStep) {}
+
+func sqlite3IdListDelete(db *sqlite3, p *IdList) {}
+
+func sqlite3ErrorMsg(p *Parse, args ...string) {}
+
+func sqlite3FinishCoding(*Parse) {}
+
+func sqlite3BeginTransaction(*Parse, int) {}
+
+func sqlite3EndTransaction(*Parse, int) {}
+
+func sqlite3Savepoint(*Parse, int, *Token) {}
+
+func sqlite3StartTable(*Parse, *Token, *Token, int, int, int, int) {}
+
+func sqlite3EndTable(*Parse, *Token, *Token, uint32, *Select) {}
+
+func sqlite3_strnicmp(textA, textB string, length int) int {
+	return 0
+}
+
+func sqlite3AddColumn(*Parse, Token, Token) {}
+
+// ./parse.go:2628:3: undefined: sqlite3ErrorMsg
